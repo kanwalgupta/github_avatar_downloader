a1aabf7117b62875afbe9e83b6825c800afc98d7
kanwalgupta <kanwal.gupta1@gmail.com>
2018-09-03 23:14:03 +0000

    Implementation for printing GITHub avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,22 @@
 var request = require('request');
+var secrets = require('./secrets.js');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+    var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization' : secrets.GITHUB_TOKEN
+    }
+  };
+  request(options, function(err, res, body) {
+    cb(err, JSON.parse(body));
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  result.forEach(function(contributor){
+    console.log(contributor.avatar_url);
+  });
 });
