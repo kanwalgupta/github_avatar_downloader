caecf0b9aa67b5d7fa2483623b0d0d2e9457b927
kanwalgupta <kanwal.gupta1@gmail.com>
2018-09-04 00:35:30 +0000

    Implementation for loading secret keys from processe.env file

--- download_avatars.js
+++ download_avatars.js
@@ -2,13 +2,14 @@ var request = require('request');
 var secrets = require('./secrets.js');
 var fs = require('fs');
 var repoInfo = process.argv.slice(2);
+require('dotenv').config();
 
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization' : secrets.GITHUB_TOKEN
+      'Authorization' : process.env.token
     }
   };
   request(options, function(err, res, body) {
